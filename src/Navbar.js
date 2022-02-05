530de3d1f0253163dd449141bb584b4b950e53f1
Vikram Sah <vikramabhishek11@gmail.com>
2022-02-04 23:37:22 -0600

    Initial commit - Created Navbar and Home Component

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Exchange Hub</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/view">View</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
